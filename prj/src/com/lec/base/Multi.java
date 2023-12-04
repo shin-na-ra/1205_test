73939e4aec6a9c0611cb8c5e0eb1620aad69ba87
shin <nas221@naver.com>
2023-12-05 08:32:45 +0900

    multi task1 branch added

---
+++ prj/src/com/lec/base/Multi.java
@@ -0,0 +1,14 @@
+package com.lec.base;
+
+public class Multi {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i <= 9; i++) {
+			for(int j=2; j<=9; j++) {
+				System.out.println(j+"X"+i+"="+j*i);
+			}
+		}
+	}
+
+}
