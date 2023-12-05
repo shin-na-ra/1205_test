a1d44efd70df9162b99f7fa0524ed0dc9d51f018
shin <nas221@naver.com>
2023-12-05 09:28:16 +0900

    2023.12.05 quiz1 : quiz1번

--- prj/src/com/lec/base/Main.java
+++ prj/src/com/lec/base/Main.java
@@ -1,10 +1,26 @@
 package com.lec.base;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test");
+		
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int sum = 0;
+		
+		System.out.print("몇개의 숫자를 입력받을까? : ");
+		num = sc.nextInt();
+		
+		for(int i = 1; i<= num; i++) {
+			System.out.print(i+"의 숫자 : ");
+			sum += sc.nextInt();
+		}
+		
+		System.out.println("총합 : "+sum);
 	}
 
 }
