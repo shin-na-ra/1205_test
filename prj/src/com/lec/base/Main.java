6d6a8665299f68573f0f996c73be696ba6fafffe
shin <nas221@naver.com>
2023-12-05 10:25:38 +0900

    2023.12.05 : 충돌 해결

--- prj/src/com/lec/base/Main.java
+++ prj/src/com/lec/base/Main.java
@@ -1,10 +1,29 @@
 package com.lec.base;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test");
+		
+		int total = 0;
+		int number = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 1; i<= number; i++) {
+			System.out.print(i+"의 숫자 : ");
+			total += sc.nextInt();
+		}
+		
+		for(int i = 1; i<= number; i++) {
+			System.out.print(i+"의 숫자 : ");
+			total += sc.nextInt();
+		}
+		
+		System.out.println("총합 : "+total);
 	}
 
 }
